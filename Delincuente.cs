8d47cf45656c3ad5797e9233f34cfed5283396a2
BryanTdesign <mendezbryan0826@gmail.com>
2022-11-09 18:44:08 -0400

    Programa compilado

---
+++ Delincuente.cs
@@ -0,0 +1,33 @@
+using System;
+
+//definir una struct
+
+struct Delincuente 
+{
+    //public string x;
+    public ulong x;
+    public double y;
+    public void delincuente (ulong numeroCedula, double montoRobado)
+    {
+        x = numeroCedula;
+        y = montoRobado;
+    }
+
+    public void mostrarDelincuente()
+    {
+        Console.WriteLine("Hola soy {0}",x);
+        Console.WriteLine("Y estoy aqui por un robito de {0}",y);
+    }
+}
+
+class MyClient
+{
+    public unsafe static void Main()
+    {
+        //MiStruct ms = new MiStruct(); //Asi es OK, tambien
+        Delincuente ms;
+        Delincuente * ps = &ms; //Declara un puntero a la struct y lo inicializa con su direccion.
+        ps->delincuente(99999, 1234.56); //accede a los miembros de la struct con el operador de punteros ->
+        ps->mostrarDelincuente();
+    }
+}
